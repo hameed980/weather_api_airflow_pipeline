2f38ec896301b5c1e2433651c25d60b1b77db4dd
abdul hameed <abdulhameedak247@gmail.com>
2025-06-07 16:03:03 +0000

    Initial commit

--- weather_dag.py
+++ weather_dag.py
@@ -1,10 +1,12 @@
-from airflow import DAG  
+from airflow import DAG
 from datetime import timedelta, datetime
-from airflow.sensors.http_sensor import HttpSensor  
-import json  
-from airflow.providers.http.operators.http import HttpOperator
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
 import pandas as pd
+
+
 
 
 def kelvin_to_fahrenheit(temp_in_kelvin):
@@ -14,12 +16,10 @@ def kelvin_to_fahrenheit(temp_in_kelvin):
 
 def transform_load_data(task_instance):
     data = task_instance.xcom_pull(task_ids="extract_weather_data")
-    data = json.loads(data) 
-
     city = data["name"]
     weather_description = data["weather"][0]['description']
     temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
-    feels_like_farenheit = kelvin_to_fahrenheit(data["main"]["feels_like"])
+    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
     min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
     max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
     pressure = data["main"]["pressure"]
@@ -44,58 +44,56 @@ def transform_load_data(task_instance):
                         }
     transformed_data_list = [transformed_data]
     df_data = pd.DataFrame(transformed_data_list)
-    aws_credentials = {"key": "XXX", "secret": "XX+XXX",
-                       "token": "XXXXXXXXX"}
+    aws_credentials = {"key": "xxxxxxxxx", "secret": "xxxxxxxxxx", "token": "xxxxxxxxxxxxxx"}
 
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y%H%M%S")
     dt_string = 'current_weather_data_portland_' + dt_string
-    df_data.to_csv(
-    f"s3://bucket_name-{dt_string}.csv",
-    index=False,
-    storage_options=aws_credentials
-    )
-    
+    df_data.to_csv(f"s3://weatherapiairflowyoutubebucket-yml/{dt_string}.csv", index=False, storage_options=aws_credentials)
+
+
+
 default_args = {
-    "owner": "abdul",
-    "start_date": datetime(2025, 6, 4),  
-    "depends_on_past": False,           
-    "retries": 2,
-    "retry_delay": timedelta(minutes=5),
-    "email": ["abdul@gmail.com"],       
-    "email_on_failure": False,
-    "email_on_retry": False             
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 8),
+    'email': ['myemail@domain.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
 
-with DAG(
-    dag_id="weather_api_dag",           
-    default_args=default_args,
-    schedule_interval="@daily",         
-    catchup=False
-) as dag:
-    
-    is_weather_api_ready = HttpSensor(
-        task_id="is_weather_api_ready",
-        http_conn_id="weathermap_api",  
-        endpoint = "/data/2.5/weather?q=karachi&appid=[api key]",
-        poke_interval = 1,    
-        timeout = 1,   
-        mode = "poke",
-        retries=0  
 
-)
-    extract_weather_data = HttpOperator(
-        task_id="extract_weather_data",
-        http_conn_id="weathermap_api",
-        endpoint="/data/2.5/weather?q=karachi&appid={api key]}",
-        method = "GET",
-        response_check=lambda r: json.loads(r.text) ,
-        log_response = True
-     )
-    
-    transform_load_weather_data = PythonOperator(
+
+with DAG('weather_dag',
+        default_args=default_args,
+        schedule_interval = '@daily',
+        catchup=False) as dag:
+
+
+        is_weather_api_ready = HttpSensor(
+        task_id ='is_weather_api_ready',
+        http_conn_id='weathermap_api',
+        endpoint='/data/2.5/weather?q=Portland&APPID=5031cde3d1a8b9469fd47e998d7aef79'
+        )
+
+
+        extract_weather_data = SimpleHttpOperator(
+        task_id = 'extract_weather_data',
+        http_conn_id = 'weathermap_api',
+        endpoint='/data/2.5/weather?q=Portland&APPID=5031cde3d1a8b9469fd47e998d7aef79',
+        method = 'GET',
+        response_filter= lambda r: json.loads(r.text),
+        log_response=True
+        )
+
+        transform_load_weather_data = PythonOperator(
         task_id= 'transform_load_weather_data',
         python_callable=transform_load_data
         )
-    
-    is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
-    
+
+
+
+
+        is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
